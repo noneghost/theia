6ad9718853b253586ef0974428e719c15c0577f9
noneghost <zcPython_crawler@yeah.net>
2025-05-26 16:49:48 +0800

    Refactor ffmpeg replacement logic to improve cache and hash checks

--- packages/debug/src/browser/model/debug-data-breakpoint.tsx
+++ packages/debug/src/browser/model/debug-data-breakpoint.tsx
@@ -1,4 +1,18 @@
-
+// *****************************************************************************
+// Copyright (C) 2019 TypeFox and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+//
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License v. 2.0 are satisfied: GNU General Public License, version 2
+// with the GNU Classpath Exception which is available at
+// https://www.gnu.org/software/classpath/license.html.
+//
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
+// *****************************************************************************
 
 import * as React from '@theia/core/shared/react';
 import { TreeElement } from '@theia/core/lib/browser/source-tree';
@@ -7,7 +21,6 @@ import { DebugBreakpoint, DebugBreakpointOptions, DebugBreakpointDecoration } fr
 import { BreakpointManager } from '../breakpoint/breakpoint-manager';
 import { nls } from '@theia/core';
 import { TREE_NODE_INFO_CLASS } from '@theia/core/lib/browser';
-
 
 export class DebugDataBreakpoint extends DebugBreakpoint<DataBreakpoint> implements TreeElement {
 
